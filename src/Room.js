f243ee95f4b86b67594cf185e7cf225217b2e20d
talha68 <66215409+talha68@users.noreply.github.com>
2020-07-08 12:20:47 +0500

    Assignment class 4

--- src/Room.js
+++ src/Room.js
@@ -4,12 +4,12 @@ import './Room.css';
 
 function Room() {
     let [isLit,setLit] = useState(false);
-    let [age,increaseAge]=useState(35)
-    function updateLit(){
-        console.log("Button Clicked");
-        setLit(!isLit);
+    let [temprature,increasedecreasetemp]=useState(72)
+    // function updateLit(){
+    //     console.log("Button Clicked");
+    //     setLit(!isLit);
     
-    }
+    // }
     // const incrementAge= function(){
     //     console.log(" IncrementAge Button Clicked");
     //     increaseAge(age++);
@@ -24,14 +24,18 @@ function Room() {
     return (
       <div className={`room ${isLit?"lit":"dark"}`}>
         This Room is : {isLit? "lit" : "dark"}
-        <button onClick={updateLit}>Toogle light</button>
-
+        <button onClick={()=>{setLit(isLit?isLit:!isLit)}}>ON</button>
+        <button onClick={()=>{setLit(isLit?!isLit:isLit)}}>OFF</button>
         <br/>
-        The age is :{age}
+        The Temprature is :{temprature}
         <button onClick={()=>{
             console.log("Increase Age button clicket")
-            increaseAge(++age)
-        }}>Increase Age</button>
+            increasedecreasetemp(++temprature)
+        }}>+</button>
+        <button onClick={()=>{
+            console.log("Increase Age button clicket")
+            increasedecreasetemp(--temprature)
+        }}>-</button>
         
       </div>
     );
